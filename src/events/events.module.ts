04e0baf0b8beec10c7e6501ddcdc4f85a4ed9d8c
SIndujan28 <30890973+SIndujan28@users.noreply.github.com>
2020-05-27 19:10:55 +0530

    import matchmaking in ws events

--- src/events/events.module.ts
+++ src/events/events.module.ts
@@ -1,7 +1,9 @@
 import { Module } from '@nestjs/common';
 import { EventsGateway } from './events.gateway';
+import { MatchmakingModule } from 'src/matchmaking/matchmaking.module';
 
 @Module({
+    imports:[MatchmakingModule],
     providers:[EventsGateway]
 })
 export class EventsModule {}
