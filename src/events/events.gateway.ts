88f86eab2840e55db6a6784123fdd641f6b3afc1
SIndujan28 <30890973+SIndujan28@users.noreply.github.com>
2020-05-30 11:46:25 +0530

    add socket events to join, create game

--- src/events/events.gateway.ts
+++ src/events/events.gateway.ts
@@ -2,19 +2,16 @@ import { WebSocketGateway, WebSocketServer, SubscribeMessage, MessageBody, WsRes
 import { from, Observable } from 'rxjs'
 import { map } from 'rxjs/operators'
 import { Server, Socket } from 'socket.io'
+import { MatchmakingService } from 'src/matchmaking/matchmaking.service';
+
 
 @WebSocketGateway(
     {namespace: 'events'}
     )
 export class EventsGateway {
+  constructor(private matchMaking:MatchmakingService) {}
     @WebSocketServer()
     server: Server;
-
-        // @SubscribeMessage('events')
-        // handleEvent(@MessageBody() data:string): string {
-        //     console.log(data)
-        //     return data;
-        // }
 
     @SubscribeMessage('events')
     findAll(@MessageBody() data: any): Observable<WsResponse<number>> {
@@ -28,23 +25,48 @@ export class EventsGateway {
     return data;
   }
 
-  @SubscribeMessage('game-data')
-  async gameData(@MessageBody() data: any) {
-
+   @SubscribeMessage('game-data')
+  async gameData(@MessageBody() {roomId,gameData}: any, @ConnectedSocket() client: Socket) {
+    client.to(roomId).emit(gameData)
+    console.log("*"+client.rooms)
   }
 
-  @SubscribeMessage('join-room')
-  async joinRoom(@MessageBody() data: string, @ConnectedSocket() client: Socket) {
-    client.join(data,(err) => {
+   @SubscribeMessage('create-game')
+  async createGame(@MessageBody() data: any) {
+    return this.matchMaking.createEvent(data).then((data1) =>{ return data1} ).catch(err => {console.log(err)})
+  }
+
+   @SubscribeMessage('autojoin-game')
+  async autoJoin(@MessageBody() data: any) {
+    return this.matchMaking.autojoinEvent(data).then((data1) =>{ return data1} ).catch(err => {console.log(err)})
+  }
+
+   @SubscribeMessage('joinById-game')
+  async joinById(@MessageBody() {userId,userAlias,eventId}: any) {
+    return this.matchMaking.joinEvent(userId,userAlias,eventId).then((data1) =>{ return data1} ).catch(err => {console.log(err)})
+  }
+
+   @SubscribeMessage('join-room')
+  async joinRoom(@MessageBody() roomId: string, @ConnectedSocket() client: Socket) {
+    client.join(roomId,(err) => {
       if(err) {
         console.log(err)
       }
     })
   }
 
-  @SubscribeMessage('leave-room')
-  async leaveRoom(@MessageBody() data: string, @ConnectedSocket() client: Socket) {
-    client.leave(data,(err) => {
+   @SubscribeMessage('cancel-game')
+  async cancelGame(@MessageBody() {userId,eventId}: any) {
+    return this.matchMaking.cancelEvent(userId,eventId).then((data1) =>{ return data1} ).catch(err => {console.log(err)})
+  }
+
+   @SubscribeMessage('game-info')
+  async getGameInfo(@MessageBody() userId: any) {
+    return this.matchMaking.getEventsFor(userId).then((data1) =>{ return data1} ).catch(err => {console.log(err)})
+  }
+   @SubscribeMessage('leave-room')
+  async leaveRoom(@MessageBody() roomId: string, @ConnectedSocket() client: Socket) {
+    client.leave(roomId,(err) => {
       if(err) {
         console.log(err)
       }
